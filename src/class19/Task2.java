b74634e4dd98b1583c7bf29c80bf861fdb01e288
Asghar Nazir <asghar@syntaxtech.com>
2021-03-24 23:08:40 -0400

    Class 19 part 2

--- src/class19/Task2.java
+++ src/class19/Task2.java
@@ -20,7 +20,7 @@ public class Task2 {
         return grade;
     }
 /*
-    Write a java program of Class Students that takes students name and 3 subject grades.
+    Write  a java program of Class Students that takes students name and 3 subject grades.
      Inside your class also have a method to Calculate Average Grade. Test Student class for 5 different students with different marks. Your program should print an average mark of each students name.
 NOTE: please use different names for instance and local variables.
      */
