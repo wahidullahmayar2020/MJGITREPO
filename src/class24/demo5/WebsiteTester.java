b4a58f0985ec027946a1ba3e6074d9d6d67b1e40
Asghar Nazir <asghar@syntaxtech.com>
2021-03-31 22:13:42 -0400

    class 24

--- src/class24/demo5/WebsiteTester.java
+++ src/class24/demo5/WebsiteTester.java
@@ -2,10 +2,41 @@ package class24.demo5;
 
 public class WebsiteTester {
     public static void main(String[] args) {
-        WebDriver webDriver=new GoogleChrome();
+        WebDriver webDriver=new FireFox();
         webDriver.openWebsite();
         webDriver.signupWebsite();
         webDriver.downloadAFileFromWebsite();
         webDriver.closeWebsite();
+
+       /* WebDriver[] webDriverArray={new GoogleChrome(),new FireFox()};
+        for (WebDriver webdriver:webDriverArray
+             ) {
+            webdriver.openWebsite();
+            webdriver.signupWebsite();
+            webdriver.downloadAFileFromWebsite();
+            webdriver.closeWebsite();
+
+        }*/
+
+
+
+
+
+
+
+
+
+       /* FireFox fireFox=new FireFox();
+        fireFox.openWebsite();
+        fireFox.signupWebsite();
+        fireFox.downloadAFileFromWebsite();
+        fireFox.closeWebsite();
+
+        GoogleChrome googleChrome=new GoogleChrome();
+        googleChrome.openWebsite();
+        googleChrome.signupWebsite();
+        googleChrome.downloadAFileFromWebsite();
+        googleChrome.closeWebsite();*/
+
     }
 }
