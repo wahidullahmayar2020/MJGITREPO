de38df33d6c73e48adf745ead07f69dd45872dcd
Asghar Nazir <asghar@syntaxtech.com>
2021-04-24 15:31:47 -0400

    review class 31

--- src/reviewclass11/ArraysListDemo.java
+++ src/reviewclass11/ArraysListDemo.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class ArraysList {
+public class ArraysListDemo {
     public static void main(String[] args) {
         ArrayList<String> cities=new ArrayList<>();
         cities.add("Marrakech");
