0686d270180fbe09409435851a21295f885d3048
Asghar Nazir <asghar@syntaxtech.com>
2021-04-25 15:13:33 -0400

    Java class 32

---
+++ src/class32/file/Demo1.java
@@ -0,0 +1,30 @@
+package class32.file;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Demo1 {
+    public static void main(String[] args) throws IOException {
+        String path="C:\\Users\\Asghar Nazir\\IdeaProjects\\JavaBatch9\\Files\\Configuration.properties";
+        System.out.println(path);
+        FileInputStream fileInputStream=new FileInputStream(path);
+        Properties properties=new Properties();
+        properties.load(fileInputStream);
+        System.out.println(properties.get("URL"));
+        System.out.println(properties.get("userName"));
+        int iterations= Integer.parseInt(properties.getProperty("loop"));
+        for (int i = 0; i < iterations; i++) {
+            System.out.println("Dynamic program");
+
+        }
+
+        FileOutputStream fileOutputStream=new FileOutputStream(path);
+        properties.setProperty("name","Ghulam");
+        properties.store(fileOutputStream,"Added new property in the file");
+
+
+    }
+}
