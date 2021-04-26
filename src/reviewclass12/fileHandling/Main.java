1e0907cc737809a6a11ff38dc972f9761b113b9b
Asghar Nazir <asghar@syntaxtech.com>
2021-04-26 19:49:43 -0400

    review class 12

--- src/reviewclass12/fileHandling/Main.java
+++ src/reviewclass12/fileHandling/Main.java
@@ -1,9 +1,18 @@
 package reviewclass12.fileHandling;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String path=System.getProperty("user.dir")+"\\Files\\Configuration.properties";
         System.out.println(path);
+        FileInputStream fileInputStream=new FileInputStream(path);
+        Properties properties=new Properties();
+        properties.load(fileInputStream);
+        System.out.println(properties.getProperty("name"));
 
 
     }
