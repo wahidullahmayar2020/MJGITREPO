4dc76fd81b5199c9c9573c4c0bef5ee09fc89645
Asghar Nazir <asghar@syntaxtech.com>
2021-05-01 11:47:25 -0400

    class 33

--- src/class33/Demo3.java
+++ src/class33/Demo3.java
@@ -1,25 +1,21 @@
 package class33;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Demo1 {
+public class Demo3 {
     public static void main(String[] args) throws IOException {
         String path="C:\\Users\\Asghar Nazir\\Desktop\\Memory\\userData.xlsx";
         FileInputStream fileInputStream=new FileInputStream(path);
         XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
         Sheet sheet=xssfWorkbook.getSheet("Sheet1");
-        Row row2=sheet.getRow(2);
-        Cell cell=row2.getCell(0);
-        System.out.println(cell);
 
 
     }
-
 }
