9423c25fbb1a21123c4955c4ddfe9d310ad04d6e
Asghar Nazir <asghar@syntaxtech.com>
2021-05-01 12:27:52 -0400

    class 33

--- src/class33/Demo3.java
+++ src/class33/Demo3.java
@@ -6,6 +6,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,23 @@ public class Demo3 {
         FileInputStream fileInputStream=new FileInputStream(path);
         XSSFWorkbook xssfWorkbook=new XSSFWorkbook(fileInputStream);
         Sheet sheet=xssfWorkbook.getSheet("Sheet1");
-
-
+        int noOfRows=sheet.getPhysicalNumberOfRows();
+        ArrayList<Map<String,String>> arrayList=new ArrayList<>();
+        for (int i = 1; i < noOfRows; i++) {
+            HashMap<String,String> map=new HashMap<>();
+            Row row=sheet.getRow(i);
+            String firstName= row.getCell(0).toString();
+            String lastName= row.getCell(1).toString();
+            String gender= row.getCell(2).toString();
+            map.put("firstName",firstName);
+            map.put("lastName",lastName);
+            map.put("gender",gender);
+            arrayList.add(map);
+           // System.out.println(firstName+" "+lastName+" "+gender);
+        }
+        System.out.println(arrayList);
+        
+        
+        
     }
 }
